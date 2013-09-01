68bfdc8ed4dbcbc2a84f1314601a29ada2803d22
George Pantazis <george@odopod.com>
2013-09-01 12:16:23 -0700

    Adding middleware loop, linting.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+'use strict';
+
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 var parseUrl = require('url').parse;
 var parseQuerystring = require('querystring').parse;
 
 var namedParam = /:\w+/g;
-var splatParam = /\*\w+/g;
 var escapeRegExp = /[-[\]{}()+?.,\\^$|#\s]/g;
 var namespaced = /^[\w\/:]+\|(\w+)$/;
 
@@ -19,7 +20,7 @@ function Ramrod(routes) {
         if (util.isRegExp(routes[path])) {
           this.routes[path] = routes[path];
 
-        } else if (typeof routes[path] == "function") {
+        } else if (typeof routes[path] === 'function') {
           this.add(path, routes[path]);
 
         } else {
@@ -33,10 +34,10 @@ function Ramrod(routes) {
 util.inherits(Ramrod, EventEmitter);
 
 Ramrod.prototype.add = function(route, name, callback) {
-  if (!callback && typeof name == "function") {
+  if (!callback && typeof name === 'function') {
     callback = name;
   }
-  if (!name || typeof name == "function") {
+  if (!name || typeof name === 'function') {
     name = route;
   }
   if (!util.isRegExp(route)) {
@@ -48,7 +49,7 @@ Ramrod.prototype.add = function(route, name, callback) {
 
   this.routeObjects[name] = [];
 
-  for ( var i in name.match(namedParam) ) {
+  for (var i in name.match(namedParam)) {
     this.routeObjects[name].push(name.match(namedParam)[i].split(':')[1]);
   }
 
@@ -59,10 +60,10 @@ Ramrod.prototype.add = function(route, name, callback) {
   var methodName = method === 'del' ? 'delete' : method;
 
   Ramrod.prototype[method] = function(route, name, callback) {
-    if (!callback && typeof name == "function") {
+    if (!callback && typeof name === 'function') {
       callback = name;
     }
-    if (!name || typeof name == "function") {
+    if (!name || typeof name === 'function') {
       name = route;
     }
     if (!util.isRegExp(route)) {
@@ -78,54 +79,81 @@ Ramrod.prototype.add = function(route, name, callback) {
 Ramrod.prototype._routeToRegExp = function(route) {
 
   route = route.replace(escapeRegExp, '\\$&')
-    .replace(namedParam, '([^\/]+)')
+    .replace(namedParam, '([^\/]+)');
 
   return new RegExp('^\/' + route + '$');
 };
 
 function next() {}
 
+var middleware = [];
+
+Ramrod.prototype.use = function(cb) {
+  middleware.push(cb);
+};
+
 Ramrod.prototype.dispatch = function(req, res) {
+  var self = this;
   var params, routeMethod;
   var url = parseUrl(req.url);
   var method = req.method && req.method.toLowerCase();
 
-  this.emit('before', req, res, next);
-
-  for (var path in this.routes) {
-    if ((params = this.routes[path].exec(url.pathname))) {
-      var args = [path, req, res];
-
-      routeMethod = namespaced.exec(path);
-
-      params = params.slice(1);
-
-      var output = {};
-
-      output.path = {};
-
-      for ( var i in params ) {
-        output.path[this.routeObjects[path][i]] = params[i]
-      }
-
-      if (url.query) {
-        output.query = parseQuerystring(url.query);
-      }
-
-      args = args.concat(output);
-
-      if (routeMethod && routeMethod[1]) {
-        if (routeMethod[1] === method) {
-          return this.emit.apply(this, args);
-        }
+  var runMiddleware = function(which) {
+    middleware[which](req, res, function() {
+      if (which === middleware.length - 1) {
+        done();
       } else {
-        return this.emit.apply(this, args);
+        runMiddleware(which + 1);
       }
+    });
+  };
 
+  var done = function() {
+
+    self.emit('before', req, res, next);
+
+    for (var path in self.routes) {
+      if ((params = self.routes[path].exec(url.pathname))) {
+        var args = [path, req, res];
+
+        routeMethod = namespaced.exec(path);
+
+        params = params.slice(1);
+
+        var output = {};
+
+        output.path = {};
+
+        for (var i in params) {
+          output.path[self.routeObjects[path][i]] = params[i];
+        }
+
+        if (url.query) {
+          output.query = parseQuerystring(url.query);
+        }
+
+        args = args.concat(output);
+
+        if (routeMethod && routeMethod[1]) {
+          if (routeMethod[1] === method) {
+            return self.emit.apply(self, args);
+          }
+        } else {
+          return self.emit.apply(self, args);
+        }
+
+      }
     }
+
+    self.emit('*', req, res, next);
+
+  };
+  if (middleware.length > 0) {
+    runMiddleware(0);
+  } else {
+    done();
   }
 
-  this.emit('*', req, res, next);
 };
 
 module.exports = function(routes) {
